0a85f6f04f745b9ec806386cd36a04081896946f
cosmicchasm <aidanvstanford@gmail.com>
2026-08-19 00:14:29 -0500

    Skeletons for pong_core as well as CMakeLists.txt update
    Aidan S

---
+++ src/pong_core.c
@@ -0,0 +1,20 @@
+/*
+ * Author: Aidan S
+ */
+
+// Remaking this file since I was very smart and deleted it forever
+
+// stdlib includes here
+#include <stdint.h>
+
+// Zephyr includes
+#include <zephyr/kernel.h>
+
+#include "pong_core.h"
+#include "oled_core.h"
+#include "graphics_core.h"
+
+// TODO: Work on filling this stuff out
+int pong_main(void) {
+  return 0;
+}
---
+++ src/pong_core.h
@@ -0,0 +1,10 @@
+/*
+ * Author: Aidan S
+ */
+
+#ifndef __PONG_CORE__
+#define __PONG_CORE__
+
+// TODO: rewrite everything
+
+#endif
